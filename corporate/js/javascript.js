0d5e9f688034507454bfc1c5abadba1f41c18119
nmarah <marahnasser6@gmail.com>
2022-12-05 18:42:44 +0200

    update navbar

--- corporate/js/javascript.js
+++ corporate/js/javascript.js
@@ -14,3 +14,14 @@ valueDisplays.forEach((valueDisplay) => {
     },duration);
     
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
