cf888e99dc43ceff664754f33b79fe0a027301cc
nmarah <marahnasser6@gmail.com>
2022-12-10 12:43:43 +0200

    updata video section

--- corporate/js/javascript.js
+++ corporate/js/javascript.js
@@ -3,50 +3,52 @@ let valueDisplays = document.querySelectorAll(".num");
 let interval = 5000;
 
 valueDisplays.forEach((valueDisplay) => {
-    let startValue = 0;
-    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-    let duration = Math.floor(interval/endValue);
-    let counter = setInterval(function(){
-        startValue += 1;
-        valueDisplay.textContent = startValue;
-        if(startValue == endValue){
-            clearInterval(counter);
-        }
-    },duration);
-    
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+
 });
 
 
 //hidden & show navbar
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";    
-      document.getElementById("navbar").style.background='#000';
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.background = '#000';
 
   } else {
     document.getElementById("navbar").style.top = "-50px";
     document.getElementById("navbar").style.backgroundColor = "transparent";
     document.getElementById("navbar").style.opacity = "1";
 
-   
+
   }
 
   prevScrollpos = currentScrollPos;
 }
 
-let btn = document.querySelector('.btn');
+let btn = document.querySelector('#video_section .btn');
 let clip = document.querySelector('.clip');
 let back = document.querySelector('.close');
 let video = document.querySelector('video');
-btn.onclick = function(){
+
+
+btn.onclick = function () {
   btn.classList.add('active');
   clip.classList.add('active');
   video.play();
-  video.currentTime=0;
+  video.currentTime = 0;
 }
-back.onclick = function(){
+back.onclick = function () {
   btn.classList.remove('active');
   clip.classList.remove('active');
   video.pause();
